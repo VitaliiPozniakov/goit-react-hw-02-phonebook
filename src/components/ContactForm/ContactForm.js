646dc7fef4817a476f307e616738d71b26188b8c
VitaliiPozniakov <vertalen89@gmail.com>
2022-07-02 14:50:01 +0300

    draft complete

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,95 +1,58 @@
 import React from 'react';
-// import { Label, LabelText, Btn, BtnWraper } from './ContactForm.styled';
 import css from './ContactForm.module.css';
 import PropTypes from 'prop-types';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
-// import styled from "@emotion/styled";
 import * as yup from 'yup';
 
-
-
-
 const schema = yup.object().shape({
-  name: yup.string().min(2).max(32).required(),
-  number: yup.string().min(6).max(16).required(),
- });
+  name: yup.string().min(2).max(32).trim().matches().required('Please enter name'),
+  number: yup.number().required('Please enter number'),
+});
 
-
- const initialValues = {
-   name: "",
-   number: "",
- }
-
-
-
- const ContactForm = ({ onSubmitProp }) => {
-
-
-  const handleSubmit = (values, actions) => {
-  const { name, number } = values;
-  onSubmitProp(name, number);
-    actions.resetForm()
-  }
-
-
-
-// const handleSubmit = evt => {
-//   evt.preventDefault();
-//   const { name, number } = this.state;
-//   this.props.onSubmitProp(name, number);
-//   this.reset();
-// };
-
-// reset = () => {
-//   this.setState({ name: '', number: '' });
-// };
-
-
-return (
-  <Formik
-  initialValues={initialValues}
-  onSubmit={handleSubmit}
-  validationSchema={schema}
-  >
-  <Form className={css.forma}>
-    <label htmlFor='name' className={css.label}>
-      <span className={css.labelText}>Name</span>
-      <Field
-        type="text"
-        name="name"
-        className={css.input}
-      />
-        <ErrorMessage name="name" component="span" className={css.error}/>
-    </label>
-    <label htmlFor='number' className={css.label}>
-      <span className={css.labelText}>Number</span>
-      <Field
-        type="tel"
-        name="number"
-        className={css.input}
-      />
-        <ErrorMessage name="number" component="span" className={css.error}/>
-    </label>
-    <div className={css.btnWraper}><button type="submit" className={css.btn}>
-      Add contact
-    </button></div>
-  </Form>
-  </Formik>
-);
+const initialValues = {
+  name: '',
+  number: '',
 };
 
-export default ContactForm
+const ContactForm = ({ onSubmitProp }) => {
+  const handleSubmit = (values, actions) => {
+    const { name, number } = values;
+    onSubmitProp(name, number);
+    actions.resetForm();
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validationSchema={schema}
+    >
+      <Form className={css.forma}>
+        <label htmlFor="name" className={css.label}>
+          <span className={css.labelText}>Name</span>
+          <Field type="text" name="name" className={css.input} />
+          <ErrorMessage name="name" component="span" className={css.error} />
+        </label>
+        <label htmlFor="number" className={css.label}>
+          <span className={css.labelText}>Number</span>
+          <Field type="tel" name="number" className={css.input} />
+          <ErrorMessage name="number" component="span" className={css.error} />
+        </label>
+        <div className={css.btnWraper}>
+          <button type="submit" className={css.btn}>
+            Add contact
+          </button>
+        </div>
+      </Form>
+    </Formik>
+  );
+};
+
+export default ContactForm;
 
 ContactForm.prototype = {
   onSubmitProp: PropTypes.func.isRequired,
 };
-
-
-
-
-
-
-
 
 // const Forma = styled(Form)`
 // display: flex;
@@ -101,4 +64,3 @@ ContactForm.prototype = {
 //     width: 700px;
 //   }
 // `;
-
