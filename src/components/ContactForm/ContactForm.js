269f5b72a67081b6f0c1981e2b81f654f0283939
VitaliiPozniakov <vertalen89@gmail.com>
2022-07-02 11:42:23 +0300

    1

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import { Form, Input, Label, LabelText, Btn } from './ContactForm.styled';
 import PropTypes from 'prop-types';
 
+
+
+
+
+
+
+
+
 class ContactForm extends Component {
   static propTypes = {
     onSubmitProp: PropTypes.func.isRequired,
